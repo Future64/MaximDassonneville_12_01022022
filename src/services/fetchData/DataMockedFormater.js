4f34d2f3fd7dbef3a807ba59f76cc56e61ba0bae
Maxim <developement.future@gmail.com>
2022-03-09 11:52:52 +0100

    Clean project and factoring but useeffect for API not efficient

--- src/services/fetchData/DataMockedFormater.js
+++ src/services/fetchData/DataMockedFormater.js
@@ -1,64 +1,87 @@
+/**
+ * Fetch USER_MAIN_DATA from mockDATA
+ * @param {Array} data
+ * @param {Number} id
+ * @returns Array
+ */
 const getUserMainData = (data, id) => {
-    const userMainData = []
-    userMainData.push(data)
-    let userMainDataMocked
+  const userMainData = []
+  userMainData.push(data)
+  let userMainDataMocked
 
-    for (let i = 0; i < userMainData[0].length; i++) {
-        if (userMainData[0][i].id == id) {
-            userMainDataMocked = userMainData[0][i]
-            break
-        }
+  for (let i = 0; i < userMainData[0].length; i++) {
+    if (userMainData[0][i].id == id) {
+      userMainDataMocked = userMainData[0][i]
+      break
     }
-    return userMainDataMocked
+  }
+  return userMainDataMocked
 }
 
+/**
+ * Fetch USER_PERFORMANCE from mockDATA
+ * @param {Array} data
+ * @param {Number} id
+ * @returns Array
+ */
 const getUserPerformance = (data, id) => {
-    const userPerformance = []
-    userPerformance.push(data)
-    let userPerformanceMocked
+  const userPerformance = []
+  userPerformance.push(data)
+  let userPerformanceMocked
 
-    for (let j = 0; j < userPerformance[0].length; j++) {
-        if (userPerformance[0][j].userId == id) {
-            userPerformanceMocked = userPerformance[0][j]
-            break
-        }
+  for (let j = 0; j < userPerformance[0].length; j++) {
+    if (userPerformance[0][j].userId == id) {
+      userPerformanceMocked = userPerformance[0][j]
+      break
     }
-    return userPerformanceMocked
+  }
+  return userPerformanceMocked
 }
 
+/**
+ * Fetch USER_AVERAGE_SESSIONS from mockDATA
+ * @param {Array} data
+ * @param {Number} id
+ * @returns Array
+ */
 const getAverageSession = (data, id) => {
-    const userAverageSession = []
-    userAverageSession.push(data)
-    let userAverageSessionMocked
+  const userAverageSession = []
+  userAverageSession.push(data)
+  let userAverageSessionMocked
 
-    for (let j = 0; j < userAverageSession[0].length; j++) {
-        if (userAverageSession[0][j].userId == id) {
-            userAverageSessionMocked = userAverageSession[0][j]
-            break
-        }
+  for (let j = 0; j < userAverageSession[0].length; j++) {
+    if (userAverageSession[0][j].userId == id) {
+      userAverageSessionMocked = userAverageSession[0][j]
+      break
     }
-    return userAverageSessionMocked
+  }
+  return userAverageSessionMocked
 }
 
+/**
+ * Fetch USER_ACTIVITY from mockDATA
+ * @param {Array} data
+ * @param {Number} id
+ * @returns Array
+ */
 const getUserActivity = (data, id) => {
-    const userActivity = []
-    userActivity.push(data)
-    let userActivityMocked
+  const userActivity = []
+  userActivity.push(data)
+  let userActivityMocked
 
-    for (let j = 0; j < userActivity[0].length; j++) {
-        // console.log(userAverageSession[0][j])
-        if (userActivity[0][j].userId == id) {
-            userActivityMocked = userActivity[0][j]
-            break
-        }
+  for (let j = 0; j < userActivity[0].length; j++) {
+    if (userActivity[0][j].userId == id) {
+      userActivityMocked = userActivity[0][j]
+      break
     }
+  }
 
-    return userActivityMocked
+  return userActivityMocked
 }
 
 export {
-    getUserMainData,
-    getUserPerformance,
-    getAverageSession,
-    getUserActivity,
+  getUserMainData,
+  getUserPerformance,
+  getAverageSession,
+  getUserActivity,
 }
--- src/services/fetchData/fetchApiService.js
+++ src/services/fetchData/fetchApiService.js
@@ -1,45 +1,65 @@
 import axios from 'axios'
 
-export const fetchMainData = async(id) => {
-    let results
-    await axios({
-            url: `http://localhost:3000/user/${id}`,
-            method: 'GET',
-        })
-        .then((response) => (results = response.data.data))
-        .catch((error) => console.log(error))
-    return results
+/**
+ * Fetch USER_MAIN_DATA from API
+ * @param {Number} id
+ * @returns Object
+ */
+export const fetchMainData = async (id) => {
+  let results
+  await axios({
+    url: `http://localhost:3000/user/${id}`,
+    method: 'GET',
+  })
+    .then((response) => (results = response.data.data))
+    .catch((error) => console.log(error))
+  return results
 }
 
-export const fetchActivity = async(id) => {
-    let results
-    await axios({
-            url: `http://localhost:3000/user/${id}/activity`,
-            method: 'GET',
-        })
-        .then((response) => (results = response.data.data))
-        .catch((error) => console.log(error))
-    return results
+/**
+ * Fetch USER_ACTIVITY from API
+ * @param {Number} id
+ * @returns Object
+ */
+export const fetchActivity = async (id) => {
+  let results
+  await axios({
+    url: `http://localhost:3000/user/${id}/activity`,
+    method: 'GET',
+  })
+    .then((response) => (results = response.data.data))
+    .catch((error) => console.log(error))
+  return results
 }
 
-export const fetchAverageSession = async(id) => {
-    let results
-    await axios({
-            url: `http://localhost:3000/user/${id}/average-sessions`,
-            method: 'GET',
-        })
-        .then((response) => (results = response.data.data))
-        .catch((error) => console.log(error))
-    return results
+/**
+ * Fetch USER_AVERAGE_SESSIONS from API
+ * @param {Number} id
+ * @returns Object
+ */
+export const fetchAverageSession = async (id) => {
+  let results
+  await axios({
+    url: `http://localhost:3000/user/${id}/average-sessions`,
+    method: 'GET',
+  })
+    .then((response) => (results = response.data.data))
+    .catch((error) => console.log(error))
+  return results
 }
 
-export const fetchPerformance = async(id) => {
-    let results
-    await axios({
-            url: `http://localhost:3000/user/${id}/performance`,
-            method: 'GET',
-        })
-        .then((response) => (results = response.data.data))
-        .catch((error) => console.log(error))
-    return results
+/**
+ * Fetch USER_PERFORMANCE from API
+ * @param {Number} id
+ * @returns Object
+ */
+export const fetchPerformance = async (id) => {
+  let results
+  await axios({
+    url: `http://localhost:3000/user/${id}/performance`,
+    method: 'GET',
+  })
+    .then((response) => (results = response.data.data))
+    .catch((error) => console.log(error))
+  return results
 }
