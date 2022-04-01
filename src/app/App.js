f7ea3fd85e95c7c9b31c71140eef9428a36f0c01
Future64 <developement.future@gmail.com>
2022-04-01 13:30:15 +0200

    page error404

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Dashboard from '../pages/dashboard/Dashboard'
 import Home from '../pages/home/Home'
-import Error404 from '../components/error404/Error404'
+import Error404 from '../pages/error404/Error404'
 
 const App = () => {
   return (
